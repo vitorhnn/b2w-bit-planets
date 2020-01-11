8908e70cdd19d79788cccd6d86b45210c64590da
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2020-01-11 17:12:43 -0300

    Initial commit
    Good old hello world.

---
+++ README.md
@@ -0,0 +1,3 @@
+# B2W BIT Planets API
+
+API proposta pela B2W-BIT como desafio para desenvolvedor back-end
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => res.send('Hello world!'));
+
+app.listen(port, () => console.log(`Hello world listening on ${port}`));
